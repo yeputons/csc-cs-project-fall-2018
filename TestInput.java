7bb4b803fff2afcb372e5ad410a078f344cd218e
Egor Suvorov <egor.suvorov@gmail.com>
2018-12-18 23:40:10 +0300

    Fix TestInput.java to be processed correctly

--- TestInput.java
+++ TestInput.java
@@ -1,17 +1,9 @@
-// Based on https://codeforces.com/contest/1093/submission/47171526
-// Problem: https://codeforces.com/contest/1093/problem/A
 import java.util.*;
 
-public class Main{
-    
+public class Main {
     public static void main(String args[])
     {
-        Scanner scan=new Scanner(System.in);
-        int n=scan.nextInt();
-        for(int i=0;i<n;i++)
-        {
-            int q=scan.nextInt();
-            System.out.println(q/2);
-        }
+        int n = 10;
+        int y = n;
     }
 }
