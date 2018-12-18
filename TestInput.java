410fb0939070f430cf7867b4fcb03d345d96846c
Egor Suvorov <egor.suvorov@gmail.com>
2018-12-19 00:12:50 +0300

    TestInput: make it compilable

--- TestInput.java
+++ TestInput.java
@@ -1,9 +1,10 @@
 import java.util.*;
 
-public class Main {
+public class TestInput {
     public static void main(String args[])
     {
         int n = 10 + (4 * 2);
         int y = (n + 2) * 3 / 2;
+        boolean foo = n + 1 == y;
     }
 }
