3c077596363a15e9a28f0c2b968f2285ada275a7
Egor Suvorov <egor.suvorov@gmail.com>
2018-12-18 23:11:49 +0300

    TranspilerCore: add IProgramPrinter

---
+++ TranspilerCore/IProgramPrinter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SyntaxTree.Nodes;
+
+namespace TranspilerCore
+{
+    public interface IProgramPrinter
+    {
+	    string PrintToString(Program program);
+    }
+}
