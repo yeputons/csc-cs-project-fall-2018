98361548a96e2aece42c2bf32a674407f0bfaa3e
Egor Suvorov <egor.suvorov@gmail.com>
2018-12-19 00:11:00 +0300

    Add binary expression to TestInput

--- TestInput.java
+++ TestInput.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String args[])
     {
-        int n = 10;
-        int y = n;
+        int n = 10 + (4 * 2);
+        int y = (n + 2) * 3 / 2;
     }
 }
