ca1ca5ec5ac9af8c2f24854cc09be8f49c9b1732
Egor Suvorov <egor.suvorov@gmail.com>
2018-12-18 23:18:20 +0300

    Move IVariableDeclaration; VariableDeclaration from JavaPlugin to SyntaxTree

--- JavaPlugin/VariablesParser.cs
+++ JavaPlugin/VariablesParser.cs
@@ -15,31 +15,6 @@ using JavaAstVariableDeclarator = com.github.javaparser.ast.body.VariableDeclara
 
 namespace JavaPlugin
 {
-	public interface IVariableDeclaration
-	{
-		string Name { get; }
-		IType Type { get; }
-	}
-
-	public class VariableDeclaration : IStatement, IVariableDeclaration
-	{
-		public VariableDeclaration(string name, IType type, IExpression initiailizer)
-		{
-			if (name == null) throw new ArgumentException("VariableDeclaration.Name should be non-null");
-			if (type == null) throw new ArgumentException("VariableDeclaration.Type should be non-null");
-			if (initiailizer != null && type != initiailizer.EvaluationType) throw new ArgumentException("VariableDeclaration.Initializer should have the same type as variable");
-			Name = name;
-			Type = type;
-			Initiailizer = initiailizer;
-			Children = new List<INode>();
-		}
-
-		public string Name { get; }
-		public IType Type { get; }
-		public IExpression Initiailizer { get; }
-		public IReadOnlyList<INode> Children { get; }
-	}
-
 	public interface IVariablesStorage
 	{
 		void Add(JavaAstVariableDeclarator key, IVariableDeclaration value);
