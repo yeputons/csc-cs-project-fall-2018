94c7680b28927349017dd8f0f1abc22b2302745f
Egor Suvorov <egor.suvorov@gmail.com>
2018-12-18 23:25:44 +0300

    Move *Constant from JavaPlugin to SyntaxTree

--- JavaPlugin/LiteralsParser.cs
+++ JavaPlugin/LiteralsParser.cs
@@ -10,50 +10,6 @@ using TranspilerInfrastructure;
 
 namespace JavaPlugin
 {
-	public class Int32Constant : EmptyNode, IExpression
-	{
-		public Int32Constant(int value)
-		{
-			Value = value;
-		}
-
-		public int Value { get; }
-		public IType EvaluationType => SInt32.Instance;
-	}
-
-	public class Int64Constant : EmptyNode, IExpression
-	{
-		public Int64Constant(long value)
-		{
-			Value = value;
-		}
-
-		public long Value { get; }
-		public IType EvaluationType => SInt64.Instance;
-	}
-
-	public class CharConstant : EmptyNode, IExpression
-	{
-		public CharConstant(char value)
-		{
-			Value = value;
-		}
-
-		public char Value { get; }
-		public IType EvaluationType => SChar.Instance;
-	}
-
-	public class BoolConstant : EmptyNode, IExpression
-	{
-		public BoolConstant(bool value)
-		{
-			Value = value;
-		}
-
-		public bool Value { get; }
-		public IType EvaluationType => SBoolean.Instance;
-	}
-
 	public static class LiteralsParser
 	{
 		public static readonly TaggedFunction<NodeParsingTag, IntegerLiteralExpr, Int32Constant> ParseInt32Literal = new TaggedFuncWrapper<NodeParsingTag, IntegerLiteralExpr, Int32Constant>(
---
+++ SyntaxTree/Nodes/Constants.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SyntaxTree.Types;
+
+namespace SyntaxTree.Nodes
+{
+	public class Int32Constant : EmptyNode, IExpression
+	{
+		public Int32Constant(int value)
+		{
+			Value = value;
+		}
+
+		public int Value { get; }
+		public IType EvaluationType => SInt32.Instance;
+	}
+
+	public class Int64Constant : EmptyNode, IExpression
+	{
+		public Int64Constant(long value)
+		{
+			Value = value;
+		}
+
+		public long Value { get; }
+		public IType EvaluationType => SInt64.Instance;
+	}
+
+	public class CharConstant : EmptyNode, IExpression
+	{
+		public CharConstant(char value)
+		{
+			Value = value;
+		}
+
+		public char Value { get; }
+		public IType EvaluationType => SChar.Instance;
+	}
+
+	public class BoolConstant : EmptyNode, IExpression
+	{
+		public BoolConstant(bool value)
+		{
+			Value = value;
+		}
+
+		public bool Value { get; }
+		public IType EvaluationType => SBoolean.Instance;
+	}
+}
